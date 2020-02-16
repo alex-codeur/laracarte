1214595519dfb9b828cec501cbfe68d08669c393
Alex Esaki <apachecordovax@gmail.com>
2020-02-16 10:40:43 +0100

    add set_active_route helper

--- app/helpers.php
+++ app/helpers.php
@@ -5,11 +5,18 @@ if (! function_exists('page_title')) {
     function page_title($title) {
 
         $base_title = "Laracarte - List of artisans";
-        
+
         if($title === '') {
             return "Laracarte - List of artisans";
         } else {
             return $title . ' | ' . "Laracarte - List of artisans";
         }
+    }
+}
+
+if (! function_exists('set_active_route')) {
+
+    function set_active_route($route) {
+        return Route::is($route) ? 'active' : '';
     }
 }
